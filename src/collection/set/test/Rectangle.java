421b1c0a2132c4d11de9c7520f88686631ba7d3c
youngchan <yckim8721@gmail.com>
2025-01-22 21:54:44 +0900

    컬렉션 프레임워크 - Set

---
+++ src/collection/set/test/Rectangle.java
@@ -0,0 +1,38 @@
+package collection.set.test;
+
+import java.util.Objects;
+
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        System.out.println("Rectangle.equals");
+        System.out.println("o = " + o);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+    @Override
+    public int hashCode() {
+        System.out.println("Rectangle.hashCode");
+        return Objects.hash(width, height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
